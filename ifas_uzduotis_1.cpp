ccd49af0fc2a300063b814fc05166e6360e9f5ea
aivaras <aivaras735@gmail.com>
2020-10-19 10:32:19 +0300

    nd

---
+++ ifas_uzduotis_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int did,maz;
+   cout<<"did= ";  cin>>did;
+    cout<<"maz= ";  cin>>maz;
+   //> < >= <= == !=
+
+   if (did==maz) {cout<<"skaiciai lygus";}
+        else  {cout<<"bandykite dar karta, skaiciai nelygus";}
+
+
+    return 0;
+}
